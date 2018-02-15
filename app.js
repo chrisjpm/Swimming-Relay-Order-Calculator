3dc64394ace4b6563bababf7addffbf3a32f1dc0
C4R15 <chrispercevalmaxwell@gmail.com>
2018-02-15 15:13:32 +0000

    Home page finished.

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var index = require('./routes/index');
 var addrecords = require('./routes/addrecords');
 var viewrecords = require('./routes/viewrecords');
 var calculator = require('./routes/calculator');
-var results = require('./routes/results');
 
 var app = express();
 var port = 80;
@@ -40,7 +39,6 @@ app.use('/', index);
 app.use('/addrecords', addrecords);
 app.use('/viewrecords', viewrecords);
 app.use('/calculator', calculator);
-app.use('/results', results);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
