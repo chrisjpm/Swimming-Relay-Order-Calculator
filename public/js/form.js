15aa7305dd40b9b49ec0fc8d5d2ed6f240e7e7e3
C4R15 <chrispercevalmaxwell@gmail.com>
2018-02-18 16:16:39 +0000

    Finished add records pagw with input validation

---
+++ public/js/form.js
@@ -0,0 +1,9 @@
+//Prevent the form submitting when accidently hitting enter
+$(document).ready(function() {
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+});
